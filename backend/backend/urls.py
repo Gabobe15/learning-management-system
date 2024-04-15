555d3eb687ee73de50afc6e4e23181640a752423
Gabobe15 <gabobe33@gmail.com>
2024-04-15 21:18:39 +0300

    customer authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin # type: ignore
+from django.urls import path # type: ignore
 
 # media and static 
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings # type: ignore
+from django.conf.urls.static import static # type: ignore
 
 urlpatterns = [
     path('admin/', admin.site.urls),
