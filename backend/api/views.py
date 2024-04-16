19dec47446828df083ecc291ec8bda3430082374
Gabobe15 <gabobe33@gmail.com>
2024-04-16 07:47:30 +0300

    reset & change password

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+import random
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from api import serializers as api_serializer 
 from userauths.models import Profile, User
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -14,3 +17,50 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class= api_serializer.RegisterSerializer
+    
+    
+def generate_random_otp(length=7):
+    otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+    return otp
+    
+class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+    
+    def get_object(self):
+        email = self.kwargs['email'] #we retrieving email from url api/users/token/gabobe@gmail.com
+        
+        user = User.objects.filter(email=email).first()
+        
+        if user: # we are looking if user exists
+            uuidb64 = user.pk
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh.access_token)
+            
+            user.refresh_token = refresh_token
+            user.otp = generate_random_otp()
+            user.save()
+            
+            link = f"http://localhost:5173/create-new-password/?otp={user.otp}&uuidb64={uuidb64}&refresh_token={refresh_token}"
+            print(f"link =====", link)
+            
+        return user
+    
+class PasswordChangeAPIView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+    
+    def create(self, request, *args, **kwargs):
+        otp = request.data['otp']
+        uuidb64 = request.data['uuidb64']
+        password = request.data['password']
+        
+        user = User.objects.get(id=uuidb64, otp=otp)
+        if user:
+            user.set_password(password)
+            user.otp = ''
+            user.save()
+            
+            return Response({"message":"Password changed successfully."}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message":" user does not exits."}, status=status.HTTP_404_NOT_FOUND)
